2fe52a58c66fd0c0005ded45f3bb4f973fb8cd4f
AnhTuan05112005 <anhtuanphan1407@gmail.com>
2024-10-01 19:51:00 +0700

    nop bai ss11/cac_bai_tap

---
+++ C0624l1/ss1/src/ss11/bai_tap/ReverseElement.java
@@ -0,0 +1,28 @@
+package ss11.bai_tap;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class ReverseElement {
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Enter value at the " + i + " position");
+            numbers[i] = scanner.nextInt();
+        }
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < numbers.length; i++) {
+            stack.push(numbers[i]);
+        }
+        int[] reverseArray = new int[5];
+        for (int i = 0; i < reverseArray.length; i++) {
+            reverseArray[i] = stack.pop();
+        }
+
+        System.out.println(Arrays.toString(reverseArray));
+    }
+}
+
